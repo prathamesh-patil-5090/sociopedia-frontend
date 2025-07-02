b454e2a89172f6adf12494253a049a16094e1564
Prathamesh Patil <122prathamesh6002@sjcem.edu.in>
2025-07-02 19:38:37 +0530

    Removed /src/scenes/googleCallback fropm App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import ForgotPassword from "scenes/loginPage/ForgotPassword";
 import Auth0Callback from "scenes/auth0Callback";
 import Auth0TestPage from "scenes/auth0Test";
 import Auth0ProfilePage from "scenes/auth0ProfilePage";
-import GoogleCallback from "scenes/googleCallback";
-import GoogleOAuthTest from "components/GoogleOAuthTest";
 import { useMemo, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -45,10 +43,8 @@ function App() {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
                 <Route path="/callback" element={<Auth0Callback />} />
-                <Route path="/auth/google/callback" element={<GoogleCallback />} />
                 <Route path="/auth0-test" element={<Auth0TestPage />} />
                 <Route path="/auth0-profile" element={<Auth0ProfilePage />} />
-                <Route path="/google-test" element={<GoogleOAuthTest />} />
                 <Route
                   path="/profile/:userId"
                   element={<ProfilePage />}
